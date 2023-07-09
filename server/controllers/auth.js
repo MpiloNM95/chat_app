882e5d5a8561e5baac7949600cd40e3eca9ff734
MpiloNM95 <mpilonmafu95@gmail.com>
2023-07-09 12:40:13 +0200

    auth js and User schema notes added to the files

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,6 +3,48 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 /* REGISTER  USER */
+// This is the register function that will be used to register a user
+// This is the async function that will be used to register a user
+// This is the try catch block that will be used to register a user
+// This is the const that will be used to destructure the request body
+// This is the const that will be used to generate a salt
+// This is the const that will be used to hash the password
+// This is the const that will be used to create a new user
+// This is the const that will be used to save the new user
+// This is the res.status that will be used to send a response
+// This is the res.json that will be used to send a response
+// export const register = async (req, res) => { means that we are using the disk storage engine
+//     try { means that we are using the disk storage engine
+//         const { means that we are using the disk storage engine
+//             firstName, means that we are using the disk storage engine
+//             lastName, means that we are using the disk storage engine
+//             email, means that we are using the disk storage engine
+//             password, means that we are using the disk storage engine
+//             picturePath, means that we are using the disk storage engine
+//             friends, means that we are using the disk storage engine
+//             location, means that we are using the disk storage engine
+//             occupation means that we are using the disk storage engine
+//         } = req.body; means that we are using the disk storage engine
+// const salt = await bcrypt.genSalt(); means that we are using the disk storage engine
+// const passwordHash = await bcrypt.hash(passowrd, salt); means that we are using the disk storage engine
+// const newUser = new User({ means that we are using the disk storage engine
+//     firstName, means that we are using the disk storage engine
+//     lastName, means that we are using the disk storage engine
+//     email, means that we are using the disk storage engine
+//     password: passwordHash, means that we are using the disk storage engine
+//     picturePath, means that we are using the disk storage engine
+//     friends, means that we are using the disk storage engine
+//     location, means that we are using the disk storage engine
+//     occupation, means that we are using the disk storage engine
+//     viewedProfile: Math.floor(Math.random() * 10000), means that we are using the disk storage engine
+//     impressions: Math.floor(Math.random() * 10000) means that we are using the disk storage engine
+// }); means that we are using the disk storage engine
+// const savedUser = await newUser.save(); means that we are using the disk storage engine
+// res.status(201).json(savedUser); means that we are using the disk storage engine
+//     } catch (err) { means that we are using the disk storage engine
+//         res.status(500).json({ error: err.message }) means that we are using the disk storage engine
+//     } means that we are using the disk storage engine
+// } means that we are using the disk storage engine
 export const register = async (req, res) => {
     try {
         const {
--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,47 @@
 import mongoose from "mongoose";
 
+// This is the schema that will be used to store our users
+// const UserSchema = new mongoose.Schema({ means that we are using the mongoose schema
+//     firstName: { means that we are using the mongoose schema
+//         type: String, means that we are using the mongoose schema
+//         required: true, means that we are using the mongoose schema
+//         min: 2, means that we are using the mongoose schema
+//         max: 50, means that we are using the mongoose schema
+//     }, means that we are using the mongoose schema
+//     lastName: { means that we are using the mongoose schema
+//         type: String, means that we are using the mongoose schema
+//         required: true, means that we are using the mongoose schema
+//         min: 2, means that we are using the mongoose schema
+//         max: 50, means that we are using the mongoose schema
+//     }, means that we are using the mongoose schema
+//     email: { means that we are using the mongoose schema
+//         type: String, means that we are using the mongoose schema
+//         required: true, means that we are using the mongoose schema
+//         max: 50, means that we are using the mongoose schema
+//         unique: true, means that we are using the mongoose schema
+//     }, means that we are using the mongoose schema
+//     password: { means that we are using the mongoose schema
+//         type: String, means that we are using the mongoose schema
+//         required: true, means that we are using the mongoose schema
+//         min: 5, means that we are using the mongoose schema
+//         max: 50, means that we are using the mongoose schema
+//     }, means that we are using the mongoose schema
+//     picturePath: { means that we are using the mongoose schema
+//         type: String, means that we are using the mongoose schema
+//         default: "", means that we are using the mongoose schema
+//     }, means that we are using the mongoose schema
+//     friends: { means that we are using the mongoose schema
+//         type: Array, means that we are using the mongoose schema
+//         default: [], means that we are using the mongoose schema
+//     }, means that we are using the mongoose schema
+//     location: String, means that we are using the mongoose schema
+//     occupation: String, means that we are using the mongoose schema
+//     viewedProfile: Number, means that we are using the mongoose schema
+//     impressions: Number, means that we are using the mongoose schema
+// }, means that we are using the mongoose schema
+// { timestamps: true } means that we are using the mongoose schema
+// const User = mongoose.model("User", UserSchema); means that we are using the mongoose schema
+// export default User; means that we are using the mongoose schema
 const UserSchema = new mongoose.Schema({
         firstName: {
             type: String,
