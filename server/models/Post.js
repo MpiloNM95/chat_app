4a1f09b6c022e494c1de23424813cc0ec64d3808
MpiloNM95 <mpilonmafu95@gmail.com>
2023-07-09 16:29:15 +0200

    post schema comments have been added to the Post js file in the models folder

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,5 +1,33 @@
 import mongoose from "mongoose";
 
+// This is the schema for our posts
+// const postSchema = mongoose.Schema({ means that we are using the mongoose schema
+// userId: { means that we are using the mongoose schema
+// type: String, means that we are using the mongoose schema
+// required: true, means that we are using the mongoose schema
+// }, means that we are using the mongoose schema
+// firstName: { means that we are using the mongoose schema
+// type: String, means that we are using the mongoose schema
+// required: true, means that we are using the mongoose schema
+// }, means that we are using the mongoose schema
+// lastName: { means that we are using the mongoose schema
+// type: String, means that we are using the mongoose schema
+// required: true, means that we are using the mongoose schema
+// }, means that we are using the mongoose schema
+// location: String, means that we are using the mongoose schema
+// description: String, means that we are using the mongoose schema
+// picturePath: String, means that we are using the mongoose schema
+// userPicturePath: String, means that we are using the mongoose schema
+// likes: { means that we are using a Map for our likes which will be a key value pair instead of an array so the it will be easier for our database to search for the required user id
+// type: Map, means that we are using a Map for our likes which will be a key value pair instead of an array so the it will be easier for our database to search for the required user id
+// of: Boolean, means that we are using a Map for our likes which will be a key value pair instead of an array so the it will be easier for our database to search for the required user id
+// }, means that we are using a Map for our likes which will be a key value pair instead of an array so the it will be easier for our database to search for the required user id
+// comments: { means that we are using an array for our comments
+// type: Array, means that we are using an array for our comments
+// default: [], means that we are using an array for our comments
+// }, means that we are using an array for our comments
+// { timestamps: true } means there will be a timestamp for when the post was created in the database
+// const Post =  mongoose.model("Post", postSchema); means that we are using the mongoose model for our post schema
 const postSchema = mongoose.Schema(
     {
         userId: {
