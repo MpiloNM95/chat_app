bbe43d1754c3f9816f6052b4d42b71429b8204d4
MpiloNM95 <mpilonmafu95@gmail.com>
2023-07-09 12:26:38 +0200

    auth js for controllers and user schema done an setup, public folder created for assets from unsplash added

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -31,8 +31,10 @@ export const register = async (req, res) => {
             viewedProfile: Math.floor(Math.random() * 10000),
             impressions: Math.floor(Math.random() * 10000)
         });
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
     } catch (err) {
-
+        res.status(500).json({ error: err.message })
     }
 }
 
